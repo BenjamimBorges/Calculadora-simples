e13b9f99b33cd07a96189756f0260dd454a61a74
BenjamimBorges <borges.benjamim@gmail.com>
2024-01-09 17:07:45 -0300

    subindo

---
+++ calc.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+janela = Tk()
+janela.title("Calculadora")
+janela.geometry("235x318")
+
+frame_tela = Frame(janela, width=235, height=50)
+
+janela.mainloop()
